96ff10e4a99d74aea826f0faa1c8335a7f2c46e7
ridwan <ridwanoseni101@gmail.com>
2020-05-08 16:27:56 -0700

    conversation-component is ready

--- resources/js/components/Conversation.vue
+++ resources/js/components/Conversation.vue
@@ -1,3 +1,64 @@
 <template>
-    
+    <div class="conversation">
+
+        <app-messages-provider :contact="selectedContact" :messages="messages" >
+        </app-messages-provider>
+
+
+        <app-contacts-list :contacts="contacts" @selected ="startConversationWith"></app-contacts-list>
+
+
+
+    </div>
 </template>
+
+<script>
+import MessagesProvider from './MessagesProvider.vue';
+import ContactsList from './ContactsList.vue';
+
+
+export default {
+    props:{
+        newMessage:{
+            type:Object,
+            default: {}
+        }
+    },
+    data: function () {
+       return {
+           contacts: [],
+           messages: [],
+           selectedContact:{},
+       }
+    },
+
+    mounted() {
+
+                axios.get('/contacts')
+                    .then((response) => {
+                        this.contacts = response.data;
+                    });
+            },
+    methods: {
+
+        startConversationWith(contact) {
+                    axios.get(`/conversation/${contact.id}`)
+                    .then((response) => {
+                        this.messages = [response.data];
+                        this.selectedContact = contact;
+                        this.$emit('selectedContact', this.selectedContact)
+                        this.saveNewMessage();
+                    })
+                },
+        saveNewMessage() {
+           setTimeout(() => {
+           }, 10000);
+        }
+
+    },
+    components: {
+        appMessagesProvider: MessagesProvider,
+        appContactsList: ContactsList,
+    }
+}
+</script>
