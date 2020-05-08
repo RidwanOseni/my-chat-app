e0b430d913998cf49326a4d1e03a0e20ae8e8b14
ridwan <ridwanoseni101@gmail.com>
2020-05-08 16:12:09 -0700

    contactslist ready

--- resources/js/components/ContactsList.vue
+++ resources/js/components/ContactsList.vue
@@ -1,3 +1,48 @@
 <template>
-    
+
+
+        <div class="contacts-list card">
+            <div class="contact card-body">
+                <ul>
+                    <li v-for="(contact, index) in contacts" :key="contact.id" @click="selectContact(index, contact)" :class="{'selected': index ==selected}">
+                        <div class="placeholder">
+                            <div class="img-case">
+                                <img src="https://via.placeholder.com/150" :alt="contact.name">
+                            </div>
+                            <div class="contact">
+                                 <p class="name">{{contact.name}}</p>
+                                 <p class="email">{{contact.email}}</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+
 </template>
+
+<script>
+export default {
+    props: {
+        contacts: {
+            type: Array,
+            default: []
+        }
+    },
+
+    data: function () {
+        return {
+            selected: 0,
+        }
+    },
+
+    methods: {
+        selectContact(index, contact) {
+            this.selected = index;
+
+            this.$emit('selected', contact);
+        }
+    }
+}
+</script>
