5c19a0f276a500282cf8650e53ee69d5096431c4
ridwan <ridwanoseni101@gmail.com>
2020-05-08 09:45:10 -0700

    chat-component ready

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,13 +19,20 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+import Vue from 'vue'
+
+import VueChatScroll from 'vue-chat-scroll'
+Vue.use(VueChatScroll)
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+
+Vue.component('app-chat', require('./components/Chat.vue'));
+Vue.component('app-header', require('./components/Header.vue'));
+Vue.component('app-message-composer', require('./components/MessageComposer.vue'));
 
 const app = new Vue({
     el: '#app',
---
+++ resources/js/components/Chat.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="container">
+
+            <app-header :contact="selectedContact"></app-header>
+            <app-conversation @selectedContact="selectContact" :newMessage="newMessage"></app-conversation>
+            <app-message-composer  @send="sendMessage"></app-message-composer>
+
+    </div>
+</template>
+
+<script>
+import Conversation from './conversation.vue';
+
+
+
+export default {
+
+
+            props: {
+                messages: {
+                type: Array,
+                default() {
+                    return []
+            }
+                },
+            },
+
+           data: function () {
+                return {
+                    selectedContact:{},
+                    contacts: [],
+                    newMessage:{
+                        "blah": "huhuhu",
+                        "text": "How far?"
+                    },
+                };
+            },
+            mounted() {
+
+                axios.get('/contacts')
+                    .then((response) => {
+                        this.contacts = response.data;
+                    });
+            },
+            methods: {
+                selectContact(value){
+
+                    this.selectedContact=value;
+                },
+                sendMessage(text) {
+            if (!this.selectedContact) {
+                return;
+            }
+
+            axios.post('/conversation/send', {
+                contact_id: this.selectedContact.id,
+                text: text
+            }).then((response) => {
+                this.newMessage = response.data;
+            })
+            .catch(err => {
+                console.log(err.response.data);
+            })
+        },
+
+            },
+
+            components: {
+                appConversation: Conversation,
+
+                }
+        }
+
+</script>
