a0e065045dbe77fd4432f5b88c0fe7e70be7ce8c
ridwan <ridwanoseni101@gmail.com>
2020-05-09 07:00:58 -0700

    messages fetching working

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Message;
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    //
+    public function getMessagesFor($id)
+    {
+        $messages = Message::where('from', $id)
+        ->orwhere('to', $id)
+        ->get();
+
+        return response()->json($messages);
+    }
+
+
+
+    public function send(Request $request)
+    {
+    $message = Message::create([
+    'from' => auth()->id(),
+    'to' => $request->contact_id,
+    'text' => $request->text
+        ]);
+
+
+
+        return response()->json($message);
+    }
 }
--- app/Message.php
+++ app/Message.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+    protected $guarded = [];
+
+    protected $appends = ['sender', 'receiver'];
+
+    public function getSenderAttribute()
+    {
+        return User::where('id', $this->from)->first();
+    }
+
+    public function getReceiverAttribute()
+    {
+        return User::where('id', $this->to)->first();
+    }
 }
