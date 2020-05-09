05edbbfcd8ab588fbab2cd459c5fbac5ba258cf7
ridwan <ridwanoseni101@gmail.com>
2020-05-08 22:53:12 -0700

    messages-provider is ready

--- resources/js/components/MessagesProvider.vue
+++ resources/js/components/MessagesProvider.vue
@@ -1,46 +1,47 @@
 <template>
-    <div class="composer">
+    <div class="msg-provider" :contact="contact" ref="provider">
 
-
-        <div class="chat-input-area">
-                    <div class="input-field card">
-                        <div class="msg-input card-body">
-
-                            <input type="text" class="input" v-model="message" @keydown.enter="send" placeholder="Message..." >
-                            <div class="input-field-icon">
-                                <i class="fas fa-plus-circle"></i>
-                                <i class="fas fa-camera"></i>
-                                <i class="fas fa-microphone"></i>
-                                <i class="fas fa-smile"></i>
-                            </div>
-
-                        </div>
-
-                    </div>
+        <div class="chat-output-box card" v-chat-scroll>
+            <div class="chat-messages" v-for="message in messages" :key="message.id" >
+                <ul v-for="item in message" :key="item.id" :class="`message${item.to == contact.id ? ' sent' : ' received'} `">
+                    <li>
+                        {{ item.text }}
+                    </li>
+                </ul>
+            </div>
         </div>
+
     </div>
 </template>
+
 <script>
+
 export default {
-    data: function () {
-        return {
-            message: ''
-        };
-    },
     props: {
         contact: {
-                type: Object,
+            type: Object,
+        },
 
-                }
+        messages: {
+            type: Array,
+            required: true
+        },
     },
     methods: {
-        send(event) {
-            event.preventDefault();
-            if (this.message == '') {
-            return;
-            }
-            this.$emit('send', this.message);
-            this.message = '';
+
+    scrollToBottom() {
+        setTimeout(()=> {
+            this.$refs.provider.scrollTop =this.$refs.provider.scrollHeight - this.$refs.provider.clientHeight;
+        }, 50);
+
+        }
+    },
+    watch: {
+        contact(contact) {
+            this.scrollToBottom();
+        },
+        messages(messages) {
+            this.scrollToBottom();
         }
     }
 }
