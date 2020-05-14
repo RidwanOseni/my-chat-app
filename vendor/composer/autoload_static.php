68953191cfdd1c3e259b4caf01d431b068c2e9a6
ridwan <ridwanoseni101@gmail.com>
2020-05-14 12:49:33 -0700

    users and messages table added

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -472,6 +472,7 @@ class ComposerStaticInit499062b4668296cd949e202e3ce285a3
 
     public static $classMap = array (
         'App\\Console\\Kernel' => __DIR__ . '/../..' . '/app/Console/Kernel.php',
+        'App\\Events\\NewMessage' => __DIR__ . '/../..' . '/app/Events/NewMessage.php',
         'App\\Exceptions\\Handler' => __DIR__ . '/../..' . '/app/Exceptions/Handler.php',
         'App\\Http\\Controllers\\Auth\\ConfirmPasswordController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/ConfirmPasswordController.php',
         'App\\Http\\Controllers\\Auth\\ForgotPasswordController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/ForgotPasswordController.php',
